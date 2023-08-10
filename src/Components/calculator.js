bb85cfb95afde5cd38534a91094158b4ab75aac7
Wils-FOURR-speed <124159862+Wils-FOURR-speed@users.noreply.github.com>
2023-08-10 10:33:14 +0100

    Add and complete calculator functionalities

--- src/Components/calculator.js
+++ src/Components/calculator.js
@@ -1,42 +1,55 @@
+import React, { useState } from 'react';
+import calculate from '../Logic/calculate';
+
 function CalcDisplay() {
+  const [displayInput, setDisplayInput] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleClicks = (event) => {
+    setDisplayInput(calculate(displayInput, event.target.textContent));
+  };
+
   return (
     <div className="App">
       <div className="calculator">
         <div className="resultScreen">
-          0
+          <p>{displayInput.next || displayInput.operation || displayInput.total || '0'}</p>
         </div>
 
         <div className="buttonSet1">
-          <button type="button">AC</button>
-          <button type="button">+/-</button>
-          <button type="button">%</button>
-          <button type="button" className="operatorBtn">÷</button>
+          <button onClick={handleClicks} type="button">AC</button>
+          <button onClick={handleClicks} type="button">+/-</button>
+          <button onClick={handleClicks} type="button">%</button>
+          <button onClick={handleClicks} type="button" className="operatorBtn">÷</button>
         </div>
 
         <div className="buttonSet2">
-          <button type="button">7</button>
-          <button type="button">8</button>
-          <button type="button">9</button>
-          <button type="button" className="operatorBtn">x</button>
+          <button onClick={handleClicks} type="button">7</button>
+          <button onClick={handleClicks} type="button">8</button>
+          <button onClick={handleClicks} type="button">9</button>
+          <button onClick={handleClicks} type="button" className="operatorBtn">x</button>
         </div>
 
         <div className="buttonSet3">
-          <button type="button">4</button>
-          <button type="button">5</button>
-          <button type="button">6</button>
-          <button type="button" className="operatorBtn">-</button>
+          <button onClick={handleClicks} type="button">4</button>
+          <button onClick={handleClicks} type="button">5</button>
+          <button onClick={handleClicks} type="button">6</button>
+          <button onClick={handleClicks} type="button" className="operatorBtn">-</button>
         </div>
 
         <div className="buttonSet4">
-          <button type="button">1</button>
-          <button type="button">2</button>
-          <button type="button">3</button>
-          <button type="button" className="operatorBtn">+</button>
+          <button onClick={handleClicks} type="button">1</button>
+          <button onClick={handleClicks} type="button">2</button>
+          <button onClick={handleClicks} type="button">3</button>
+          <button onClick={handleClicks} type="button" className="operatorBtn">+</button>
         </div>
         <div className="buttonSet5">
-          <button type="button" className="bigger">0</button>
-          <button type="button">.</button>
-          <button type="button" className="operatorBtn">=</button>
+          <button onClick={handleClicks} type="button" className="bigger">0</button>
+          <button onClick={handleClicks} type="button">.</button>
+          <button onClick={handleClicks} type="button" className="operatorBtn">=</button>
         </div>
       </div>
     </div>
