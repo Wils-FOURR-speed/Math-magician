c77e6a195d138fb7ee24ee6116d88417c1801869
Wils-FOURR-speed <124159862+Wils-FOURR-speed@users.noreply.github.com>
2023-08-12 00:38:24 +0100

    Complete Quote fetching

---
+++ src/Components/quote.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+
+function ShowQuotes() {
+  const [showQuote, setShowQuote] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const getQuotes = async () => {
+    try {
+      const response = await fetch('https://api.api-ninjas.com/v1/quotes?category=success', {
+        headers: {
+          'X-Api-Key': 'bkz8prsJU2WZD2MQRKH+/g==IVDJaiWsEnD8EZxu',
+        },
+      });
+      const text = await response.json();
+      const { quote } = text[0];
+      setShowQuote(quote);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error.message);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getQuotes();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return (
+      <p className="quote">
+        <span>Error:</span>
+        <br />
+        <h4>{error}</h4>
+      </p>
+    );
+  }
+
+  return (
+    <div className="quotes">
+      <b>Quote:</b>
+      <br />
+      {`"${showQuote}"`}
+    </div>
+  );
+}
+
+export default ShowQuotes;
